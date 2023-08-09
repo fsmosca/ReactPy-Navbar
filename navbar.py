24109c0c09c47c731d246fac7da4a0d423595cc3
fsmosca <fsmosca@users.noreply.github.com>
2023-08-09 18:15:16 +0800

    Updates bootstrap
    * Add bootstrap script integrity attribute.
    * Use bootstrap 5.2.3 css

--- navbar.py
+++ navbar.py
@@ -15,10 +15,10 @@ from fastapi import FastAPI
 BOOTSTRAP_CSS = html.link(
     {
         'rel': 'stylesheet',
-        'href': 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/'
-                'dist/css/bootstrap.min.css',
-        'integrity': 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Y'
-                     'z1ztcQTwFspd3yD65VohhpuuCOmLASjC',
+        'href': 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/'
+                 'dist/css/bootstrap.min.css',
+        'integrity': 'sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80'
+                     'zW1RWuH61DGLwZJEdK2Kadq2F9CUG65',
         'crossorigin': 'anonymous'
     }
 )
@@ -27,6 +27,8 @@ BOOTSTRAP_SCRIPT = html.script(
     {
         'src': 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/'
                'dist/js/bootstrap.bundle.min.js',
+        'integrity': 'sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9'
+                     'F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4',
         'crossorigin': 'anonymous'
     }
 )
